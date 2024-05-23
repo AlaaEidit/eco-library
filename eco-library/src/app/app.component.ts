3b61506b49b3603dcba82ea57ed95ee981322246
yg Noah <n.duchemann@it-students.fr>
2024-05-23 10:51:07 +0200

    créations des components + routing + page sign-up

--- eco-library/src/app/app.component.ts
+++ eco-library/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [HomeComponent, RouterModule],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'eco-library';
 }
+ 
--- eco-library/src/app/app.routes.ts
+++ eco-library/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home Page',
+    },
+    {
+        path: 'sign-up',
+        component: SignUpComponent,
+        title: 'Sign-Up PAge',
+    }
+];
+
+export default routeConfig;
