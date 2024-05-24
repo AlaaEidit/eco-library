21822048238ec86cafbd9c7c389c8a2b8fcea5ec
alalEid <aedalaa36@gmail.com>
2024-05-24 16:50:50 +0200

    sratr header

--- eco-library/src/app/app.routes.ts
+++ eco-library/src/app/app.routes.ts
@@ -1,6 +1,15 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'; // importing my login componrnt
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:'login',  component : LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
