30acdae09e7f9efe190e8f4d596bf48efa50a485
yg Noah <n.duchemann@it-students.fr>
2024-05-24 10:13:04 +0200

    page de products terminée

--- eco-library/src/app/app.routes.ts
+++ eco-library/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProductsComponent } from './products/products.component';
 
 export const routeConfig: Routes = [
     {
@@ -12,6 +13,11 @@ export const routeConfig: Routes = [
         path: 'sign-up',
         component: SignUpComponent,
         title: 'Sign-Up PAge',
+    },
+    {
+        path : 'products',
+        component: ProductsComponent,
+        title: 'Products Page',
     }
 ];
 
