91f405d1f611023a12a9c65834ed21a36fc10984
alalEid <aedalaa36@gmail.com>
2024-05-27 14:23:43 +0200

    create book page

--- eco-library/src/app/app.component.ts
+++ eco-library/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RouterLink,RouterLinkActive, HomeComponent],
+  imports: [RouterOutlet, LoginComponent, RouterLink,RouterLinkActive, HomeComponent,BookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- eco-library/src/app/app.routes.ts
+++ eco-library/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  {path: 'book',component: BookComponent}
 ];
 
 @NgModule({
